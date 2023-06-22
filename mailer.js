f19571e5f9548342cb308e7cee38ae3edc3fb02f
balkrishnray <balkrishnaroy31@gmail.com>
2023-06-22 14:40:53 +0545

    npm qrcode

--- mailer.js
+++ mailer.js
@@ -5,19 +5,28 @@ package require nodemailer initializer; transporter =>GMAIL
 transporter.sendMail()
 */
 const nodemailer = require('nodemailer');
-const mailOptions = {};
-const tranport = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-  port: 25,
-  secure: false,
+const mailOptions = {
+    from: '"Balkrishna Roy" <balkrishnaroy31@gmail.com>', // sender address
+    to: "balkrishnaroy31@gmail.com", // list of receivers
+    subject: "MERN STACK", // Subject line
+    text: "Hello ", // plain text body
+    html: "<b>hello from himalaya college of engineering</b>", // html body
+
+};
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     // TODO: replace `user` and `pass` values from <https://forwardemail.net>
     user: 'balkrishnaroy31@gmail.com',
-    pass: '',
+    pass: 'tyyxmeckouvtaeda',
   },
 });
 
 const mailer = (mailOptions,transporter) => {
+    console.log("sending email...");
+  
     transporter.sendMail(mailOptions,(err,info) =>{
         if(err){
             console.log(err);
@@ -27,7 +36,20 @@ const mailer = (mailOptions,transporter) => {
             }
         });
     };
-    
+    mailer(mailOptions,transporter).catch(console.error);
+//     const nodemailer = require('nodemailer');
+//     const mailOptions = {
 
+//     };
 
+// const mailer = async (mailOptions, transporter) => {
+//     transporter.sendMail(mailOptions, (err, info) => {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             console.log('Email sent:'+info.response);
+//         }
+//     });
+// };
 
+// mailer(mailOptions, transporter).catch(console.error);
